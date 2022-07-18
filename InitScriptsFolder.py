3cdb5c6f8816144700ad8f2e50e6f52564903e94
Gangxin-Li <105022948+Gangxin-Li@users.noreply.github.com>
2022-07-18 03:37:41 +0100

    2
    initscriptsfolder

--- InitScriptsFolder.py
+++ InitScriptsFolder.py
@@ -5,27 +5,29 @@ Created on Sun Jul 17 22:57:43 2022
 
 @author: gangxinli
 """
-
+import shutil
 import os
+import time
 from InitParams import InitParams
 
-Params = InitParams()
+
 def InitScriptsFolder(Params):
-    scriptspath = Params['destinationPath'] + 'scripts'
+    scriptspath = Params['destinationPath'] + 'scripts'+Params['slash']
     if not os.path.exists(scriptspath):
         os.makedirs(scriptspath)
     else:
-        file_source = 'Path/Of/Directory'
-        file_destination = 'Path/Of/Directory'
- 
-        get_files = os.listdir(file_source)
- 
-        for g in get_files:
-            shutil.move(file_source + g, file_destination)
+        localtime = time.localtime(time.time())
+        file_destination = Params['destinationPath'] + str(localtime[0:6])
+        get_files = os.listdir(scriptspath)
+        if len(get_files)!=0:
+            os.makedirs(file_destination)
+            for g in get_files:
+                shutil.move(scriptspath + g, file_destination)
     
     
     log_path = scriptspath
     return log_path
 
 if __name__ == '__main__':
+    Params = InitParams()
     initfolder = InitScriptsFolder(Params)
