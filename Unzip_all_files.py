c6471675d661742bc5bb575a7fb9523033fd79c2
Gangxin-Li <105022948+Gangxin-Li@users.noreply.github.com>
2022-07-22 21:23:57 +0100

    Un_zip
    un zip all files

---
+++ Unzip_all_files.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 21:19:01 2022
+
+@author: gangxinli
+"""
+import gzip
+import os
+
+def un_gz(file_name):
+    """
+    Input -- file path
+    
+    Result:
+        Get Unzip file on the same path
+    """
+    f_name = file_name.replace(".gz", "")
+    g_file = gzip.GzipFile(file_name)
+    open(f_name, "wb+").write(g_file.read())
+    g_file.close()
+
+
+def un_zip(path = './../Data/INDI_Lite_NIFTI'):
+    """
+    Input -- file path, a folder contais all of the data
+    
+    Result:
+        Unzip all the gz file among the path
+    """
+    print("Unziping....")
+    for root,dirs,files in os.walk(path):
+        #print("11111root:"+root) 
+        #print(dirs)
+        for file in files:
+            if file.split(".")[-1] == 'gz':
+                #print("222root file: "+os.path.join(root,file))
+                un_gz(os.path.join(root,file))
+    print("Unzip complete!")
+
+
+
+if __name__ == "__main__":
+    un_zip("/Users/gangxinli/Desktop/Internship/Neuro/Neuro_ISC/Data/movie_8")
+
+
+
+
